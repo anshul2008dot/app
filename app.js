2b17405b25bcd40cc926b5da089274551a179274
anshul2008dot <ansh08b@gmail.com>
2023-06-20 21:20:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// Study Timer
+function startStudyTimer() {
+    // Start the study timer logic
+  }
+  
+  function stopStudyTimer() {
+    // Stop the study timer logic
+  }
+  
+  // Todo List
+  function addTask() {
+    // Add a task to the todo list
+  }
+  
+  function deleteTask() {
+    // Delete a task from the todo list
+  }
+  
+  // History of Time Studied
+  function getStudyHistory() {
+    // Retrieve the history of time studied
+  }
+  
